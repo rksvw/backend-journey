faeb18d9686b9aafae2b0a7b19cd71c4c03f874f
rksvw <ritik.vw98@gmail.com>
2025-08-05 18:29:06 +0530

    source expressjs.com middleware test completed

--- test.js
+++ test.js
@@ -1,27 +1,69 @@
 const express = require("express");
 const app = express();
+const route = express.Router();
 
 app.use(express.json());
 
-
 function logged(req, res, next) {
   console.log("LOGGED");
-  next()
+  next();
 }
 
-const requestTime = function(req, res, next) {
-  req.requestTime = Date.now()
-  req.msgName = "Hello World"
-  next()
+const requestTime = function (req, res, next) {
+  req.requestTime = Date.now();
+  req.msgName = "Hello World";
+  next();
+};
+
+app.use(requestTime);
+
+function logOriginalUrl(req, res, next) {
+  console.log("Request URL:", req.originalUrl);
+  next();
 }
 
-app.use(requestTime)
+function logMethod(req, res, next) {
+  console.log("Request Type:", req.method);
+  next();
+}
 
-app.get('/cat', (req, res) => {
-  let responseText = 'Hello World!<br>'
-  responseText += `<small>Requested at: ${new Date(req.requestTime).toDateString()}</small><br><h3>${req.msgName}</h3>`;
+const logStuff = [logOriginalUrl, logMethod];
+
+route.post(
+  "/user/:id",
+  logStuff,
+  (req, res, next) => {
+    // if the user ID is 0, skip to the next route
+    if (req.params.id === "0") next("route");
+    // otherwise pass the control to the next middleware function in this stack
+    else next();
+  },
+  (req, res, next) => {
+    // send a regular response
+    res.json({ msg: "successful to regular" });
+  }
+);
+
+route.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ msg: "Something broke!" });
+});
+
+route.get("/user/:id", (req, res, next) => {
+  console.log("Request Type", req.method);
+  res.json({ msg: "successful to special" });
+});
+
+app.get("/cat", (req, res) => {
+  let responseText = "Hello World!<br>";
+  req.cookies = { msg: "hello" };
+
+  console.log("Cookies", req.cookies);
+  responseText += `<small>Requested at: ${new Date(
+    req.requestTime
+  ).toDateString()}</small><br><h3>${req.msgName}</h3>`;
   res.send(responseText);
-})
+});
 
 app.get("/", (req, res) => {
   res.send("HELLO");
@@ -30,3 +72,5 @@ app.get("/", (req, res) => {
 app.listen(4000, () => {
   console.log("SErver is listening on port 4000");
 });
+
+app.use("/", route);

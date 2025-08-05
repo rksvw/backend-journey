d3d4e0bc22d998a2ada80f4350ad3204191e68c6
rksvw <ritik.vw98@gmail.com>
2025-08-05 16:45:08 +0530

    source expressjs.com middleware test phase 1

--- test.js
+++ test.js
@@ -3,7 +3,25 @@ const app = express();
 
 app.use(express.json());
 
-const testUrl = "http://localhost:3000/name";
+
+function logged(req, res, next) {
+  console.log("LOGGED");
+  next()
+}
+
+const requestTime = function(req, res, next) {
+  req.requestTime = Date.now()
+  req.msgName = "Hello World"
+  next()
+}
+
+app.use(requestTime)
+
+app.get('/cat', (req, res) => {
+  let responseText = 'Hello World!<br>'
+  responseText += `<small>Requested at: ${new Date(req.requestTime).toDateString()}</small><br><h3>${req.msgName}</h3>`;
+  res.send(responseText);
+})
 
 app.get("/", (req, res) => {
   res.send("HELLO");
